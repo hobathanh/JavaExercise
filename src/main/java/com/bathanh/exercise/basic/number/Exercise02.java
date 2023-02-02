37b8fde9c53eba8f406c20a44037f5a64cd90e32
hobathanh <hobathanh201@gmail.com>
2023-02-02 14:49:47 +0700

    fix resolve conversation ex02

--- src/main/java/com/bathanh/exercise/basic/number/Exercise02.java
+++ src/main/java/com/bathanh/exercise/basic/number/Exercise02.java
@@ -2,13 +2,12 @@ package com.bathanh.exercise.basic.number;
 
 public class Exercise02 {
 
-    public int calculateSumOfDigits(final int a){
-        int Input = a;
-        int sum = 0, count = 0;
-        while (Input != 0){
-            count = Input % 10;
-            sum += count;
-            Input = Input / 10;
+    public int calculateSumOfDigits(final int a) {
+        int input = a;
+        int sum = 0;
+        while (input != 0){
+            sum += input % 10;
+            input /= 10;
         }
         return sum;
     }
