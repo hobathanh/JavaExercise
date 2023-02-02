0039ba18b267d02cb69f8ff04f818d0d1f8db9a7
hobathanh <hobathanh201@gmail.com>
2023-02-02 15:28:36 +0700

    add ex03

--- src/main/java/com/bathanh/exercise/basic/number/Exercise03.java
+++ src/main/java/com/bathanh/exercise/basic/number/Exercise03.java
@@ -4,24 +4,43 @@ import java.util.List;
 
 public class Exercise03 {
 
-    public String analysisOutPrimeNumbers (final int a){
+    public String analysisOutPrimeNumbers(final int a) {
         int n = a;
-        String arr = "";
-        if(n < 2){
-            System.out.println("Input khong hop le");
+        final StringBuilder arr = new StringBuilder();
+        if(n < 2) {
+            return "Input khong hop le";
         }
         else {
-            for(int i = 2; i < n; i++){
-                while (n % i == 0){
+            for(int i = 2; i < n; i++) {
+                while (isPrimeNumber(i) && n % i == 0) {
                     n = n / i;
-//                    arr.add(i);
-                    System.out.print(i + " * ");
+                    if(n == 1) {
+                        arr.append(i);
+                    }
+                    else {
+                        arr.append(i + " * ");
+                    }
                 }
-                if (n == 1){
+                if (n == 1) {
                     break;
                 }
             }
         }
-        return arr;
+        return arr.toString();
+    }
+
+    public static boolean isPrimeNumber(final int n) {
+        int primeN = n;
+        if (primeN < 2) {
+            return false;
+        }
+
+        int squareRoot = (int) Math.sqrt(primeN);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (primeN % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/com/bathanh/exercise/basic/number/Exercise03Test.java
+++ src/test/java/com/bathanh/exercise/basic/number/Exercise03Test.java
@@ -10,8 +10,10 @@ class Exercise03Test {
     void analysisOutPrimeNumbers_Ok() {
         final var Exercise03 = new Exercise03();
 
-        assertEquals("2*2*2",Exercise03.analysisOutPrimeNumbers(8));
-        assertEquals("2*2",Exercise03.analysisOutPrimeNumbers(4));
+        assertEquals("2 * 2 * 2",Exercise03.analysisOutPrimeNumbers(8));
+        assertEquals("2 * 2",Exercise03.analysisOutPrimeNumbers(4));
+        assertEquals("2 * 2 * 2 * 3 * 5 * 5",Exercise03.analysisOutPrimeNumbers(600));
+        assertEquals("Input khong hop le",Exercise03.analysisOutPrimeNumbers(1));
 
     }
 }
