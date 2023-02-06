07edf134aa3cd6dc4d0eda09415b8b0c811a2f20
hobathanh <hobathanh201@gmail.com>
2023-02-06 10:44:45 +0700

    create and add ex08

--- src/main/java/com/bathanh/exercise/basic/number/Exercise08.java
+++ src/main/java/com/bathanh/exercise/basic/number/Exercise08.java
@@ -2,19 +2,17 @@ package com.bathanh.exercise.basic.number;
 
 public class Exercise08 {
 
+
     public int reverseBits(final int n) {
         int inputN = n;
         int reverseN = 0;
         while (inputN > 0) {
             reverseN <<= 1;
-            if ((int) (n & 1) == 1)
+            if ((inputN & 1) == 1) {
                 reverseN ^= 1;
-
-            // bitwise right shift
-            //'n' by
+            }
             inputN >>= 1;
         }
-        // required number
-        return 0;
+        return reverseN;
     }
 }
