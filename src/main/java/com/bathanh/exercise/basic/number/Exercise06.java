424cb688a7fba3a81065c93f8580cb5ac1bde3a2
hobathanh <hobathanh201@gmail.com>
2023-02-06 09:03:53 +0700

    fix resolved ex06

--- src/main/java/com/bathanh/exercise/basic/number/Exercise06.java
+++ src/main/java/com/bathanh/exercise/basic/number/Exercise06.java
@@ -1,17 +1,20 @@
 package com.bathanh.exercise.basic.number;
 
 public class Exercise06 {
-    public double calculateSqrt (final double a) {
+
+    public double calculateSqrt(final double a) {
         double inputA = a;
         double result = 1.0d;
         double epsilon = 0.0001d;
         if (inputA < 0) {
             throw new IllegalArgumentException();
-        } else if (inputA == 0) {
+        }
+        if (inputA == 0) {
             return 0;
         }
-        while (Math.abs(result * result - inputA) / inputA  >= epsilon)
-            result = (inputA / result  - result) / 2 + result;
-        return (double) Math.round(result * 10000d)/10000d ;
+        while (Math.abs(result * result - inputA) / inputA >= epsilon) {
+            result = (inputA / result - result) / 2 + result;
+        }
+        return result;
     }
 }
--- src/test/java/com/bathanh/exercise/basic/number/Exercise06Test.java
+++ src/test/java/com/bathanh/exercise/basic/number/Exercise06Test.java
@@ -2,7 +2,8 @@ package com.bathanh.exercise.basic.number;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class Exercise06Test {
 
@@ -12,7 +13,7 @@ class Exercise06Test {
 
         assertThrows(IllegalArgumentException.class, () -> Exercise06.calculateSqrt(-5));
         assertEquals(0, Exercise06.calculateSqrt(0));
-        assertEquals(3.0001, Exercise06.calculateSqrt(9));
-        assertEquals(8.1609, Exercise06.calculateSqrt(66.6));
+        assertEquals(8.16088230524, Exercise06.calculateSqrt(66.6), 0.0001);
+        assertEquals(3, Exercise06.calculateSqrt(9), 0.0001);
     }
 }
