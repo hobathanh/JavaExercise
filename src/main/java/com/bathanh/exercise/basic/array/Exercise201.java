82c5b1fc31cd8597b4b0086b50c688ec88e4fef2
hobathanh <hobathanh201@gmail.com>
2023-02-06 16:29:02 +0700

    fix conversation

--- src/main/java/com/bathanh/exercise/basic/array/Exercise201.java
+++ src/main/java/com/bathanh/exercise/basic/array/Exercise201.java
@@ -2,13 +2,13 @@ package com.bathanh.exercise.basic.array;
 
 public class Exercise201 {
 
-    public int searchArrayLargest(int[] arrN) {
-        int maxArrN = arrN[0];
-        for (int i = 0; i <= arrN.length - 1; i++) {
-            if (arrN[i] > maxArrN) {
-                maxArrN = arrN[i];
+    public int searchArrayLargest(final int[] array) {
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
             }
         }
-        return maxArrN;
+        return max;
     }
 }
