135deb2c838393693c0af5ee45ca5cb71366ec71
hobathanh <hobathanh201@gmail.com>
2023-02-07 15:10:12 +0700

    fix codecov ex207

--- src/main/java/com/bathanh/exercise/basic/array/Ex207.java
+++ src/main/java/com/bathanh/exercise/basic/array/Ex207.java
@@ -7,24 +7,41 @@ public class Ex207 {
         int[] newArray = new int[array.length + 1];
         int j = 0;
         int i = 0;
-        for (i = 0; i < newArray.length; i++) {
-            if (newArray.length - i == 1) {
-                newArray[i] = a;
+//        for (i = 0; i < newArray.length; i++) {
+//            if (newArray.length - i == 1) {
+//                newArray[i] = a;
+//                break;
+//            }
+//            if (a <= array[i]) {
+//                newArray[i] = a;
+//                break;
+//            } else {
+//                newArray[i] = array[j++];
+//            }
+//        }
+        int pos = 0;
+        while (pos <= newArray.length - 1) {
+            if ((newArray.length - pos == 1) || a <= array[pos]) {
+                newArray[pos] = a;
                 break;
             }
-            if (a <= array[i]) {
-                newArray[i] = a;
-                break;
-            } else {
-                newArray[i] = array[j++];
-            }
+            newArray[pos] = array[i++];
+            pos++;
         }
-        for (int k = i + 1; k < newArray.length; k++) {
-            newArray[k] = array[j++];
+
+        for (int k = pos + 1; k < newArray.length; k++) {
+            newArray[k] = array[i++];
         }
         for (i = 0; i < newArray.length; i++) {
             resultArray.append(newArray[i] + ", ");
         }
+
+//        for (int k = i + 1; k < newArray.length; k++) {
+//            newArray[k] = array[j++];
+//        }
+//        for (i = 0; i < newArray.length; i++) {
+//            resultArray.append(newArray[i] + ", ");
+//        }
         return resultArray.substring(0, resultArray.length() - 2);
     }
 }
