d4e0a183751e0b312a13998bd5bba996281fd793
hobathanh <hobathanh201@gmail.com>
2023-02-07 16:22:17 +0700

    fix codecov ex207

--- src/main/java/com/bathanh/exercise/basic/array/Ex207.java
+++ src/main/java/com/bathanh/exercise/basic/array/Ex207.java
@@ -5,43 +5,37 @@ public class Ex207 {
     public String sortArrayAsc(final int[] array, final int a) {
         StringBuilder resultArray = new StringBuilder();
         int[] newArray = new int[array.length + 1];
+        int index = indexArray(array, a);
         int j = 0;
-        int i = 0;
-//        for (i = 0; i < newArray.length; i++) {
-//            if (newArray.length - i == 1) {
-//                newArray[i] = a;
-//                break;
-//            }
-//            if (a <= array[i]) {
-//                newArray[i] = a;
-//                break;
-//            } else {
-//                newArray[i] = array[j++];
-//            }
-//        }
-        int pos = 0;
-        while (pos <= newArray.length - 1) {
-            if ((newArray.length - pos == 1) || a <= array[pos]) {
-                newArray[pos] = a;
-                break;
+        for (int i = 0; i <= newArray.length - 1; i++) {
+            if (i == index) {
+                newArray[index] = a;
+            } else {
+                newArray[i] = array[j++];
             }
-            newArray[pos] = array[i++];
-            pos++;
         }
-
-        for (int k = pos + 1; k < newArray.length; k++) {
-            newArray[k] = array[i++];
-        }
-        for (i = 0; i < newArray.length; i++) {
+        for (int i = 0; i < newArray.length; i++) {
             resultArray.append(newArray[i] + ", ");
         }
-
-//        for (int k = i + 1; k < newArray.length; k++) {
-//            newArray[k] = array[j++];
-//        }
-//        for (i = 0; i < newArray.length; i++) {
-//            resultArray.append(newArray[i] + ", ");
-//        }
         return resultArray.substring(0, resultArray.length() - 2);
+    }
+
+    public int indexArray(int[] array, int a) {
+        int index = 0;
+        int lengthArray = array.length;
+        if (lengthArray == 0) {
+            return index = 0;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (a <= array[i]) {
+                index = i;
+                return index;
+            }
+            if (array.length - i == 1) {
+                index = i + 1;
+                return index;
+            }
+        }
+        return index;
     }
 }
--- src/test/java/com/bathanh/exercise/basic/array/Ex207Test.java
+++ src/test/java/com/bathanh/exercise/basic/array/Ex207Test.java
@@ -17,4 +17,15 @@ class Ex207Test {
         assertEquals("1, 2", ex207.sortArrayAsc(new int[]{1}, 2));
         assertEquals("1", ex207.sortArrayAsc(new int[]{}, 1));
     }
+
+    @Test
+    void indexArray_Ok() {
+        final var ex207 = new Ex207();
+
+        assertEquals(2, ex207.indexArray(new int[]{1, 2, 6, 7}, 3));
+        assertEquals(0, ex207.indexArray(new int[]{3, 5, 6, 7}, 2));
+        assertEquals(1, ex207.indexArray(new int[]{1, 2, 3, 6}, 2));
+        assertEquals(4, ex207.indexArray(new int[]{1, 2, 3, 6}, 8));
+        assertEquals(0, ex207.indexArray(new int[]{}, 1));
+    }
 }
