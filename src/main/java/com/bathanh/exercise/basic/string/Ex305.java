b8d317a67176d3ff43dfd7ee1c6b54cf9ab407b2
hobathanh <hobathanh201@gmail.com>
2023-02-08 17:52:16 +0700

    create ex305

---
+++ src/main/java/com/bathanh/exercise/basic/string/Ex305.java
@@ -0,0 +1,11 @@
+package com.bathanh.exercise.basic.string;
+
+public class Ex305 {
+
+    public boolean checkBarcode (final String n) {
+        if (n.length() > 12) {
+            return false;
+        }
+        for (int i = 0; i < )
+    }
+}
