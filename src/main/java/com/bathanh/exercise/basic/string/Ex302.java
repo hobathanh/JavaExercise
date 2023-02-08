7d278e26be0d3a42ff18460e7b8235f29700a54a
hobathanh <hobathanh201@gmail.com>
2023-02-08 14:16:01 +0700

    add ex302

---
+++ src/main/java/com/bathanh/exercise/basic/string/Ex302.java
@@ -0,0 +1,31 @@
+package com.bathanh.exercise.basic.string;
+
+public class Ex302 {
+
+    public boolean checkSymmetricString(String s) {
+        int midIndex;
+        int length = s.length();
+
+        if (length == 0) {
+            return true;
+        }
+        if (length % 2 == 0) {
+            midIndex = length / 2;
+        } else {
+            midIndex = length / 2 + 1;
+        }
+
+        int indexString1 = 0;
+        int indexString2 = length - 1;
+        while (indexString1 < midIndex && indexString2 >= midIndex) {
+            if (s.charAt(indexString1) == s.charAt(indexString2)) {
+                indexString1 += 1;
+                indexString2 -= 1;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
---
+++ src/test/java/com/bathanh/exercise/basic/string/Ex302Test.java
@@ -0,0 +1,18 @@
+package com.bathanh.exercise.basic.string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Ex302Test {
+
+    @Test
+    void checkSymmetricString_Ok() {
+        final var ex302 = new Ex302();
+
+        assertEquals(true, ex302.checkSymmetricString("abba"));
+        assertEquals(true, ex302.checkSymmetricString("abdba"));
+        assertEquals(true, ex302.checkSymmetricString("0"));
+        assertEquals(false, ex302.checkSymmetricString("abcabc"));
+    }
+}
