0f6dd3389547ae845765bcfb58fbf361078a135e
hobathanh <hobathanh201@gmail.com>
2023-02-08 11:20:31 +0700

    fix ex05

--- src/main/java/com/bathanh/exercise/basic/number/Exercise05.java
+++ src/main/java/com/bathanh/exercise/basic/number/Exercise05.java
@@ -3,11 +3,11 @@ package com.bathanh.exercise.basic.number;
 public class Exercise05 {
 
     public int sumOfNumbers(final int n) {
-        int total = 0;
         if (n < 0) {
             throw new IllegalArgumentException();
         }
-        for (int i = 0; i <= n; i++) {
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
             total += i * (i + 1);
         }
 
