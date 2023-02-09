024e7d8f77f1e33734af4c266e7df1326671f741
hobathanh <hobathanh201@gmail.com>
2023-02-09 17:23:02 +0700

    fix conversation

--- src/main/java/com/bathanh/exercise/basic/string/Ex310.java
+++ src/main/java/com/bathanh/exercise/basic/string/Ex310.java
@@ -4,17 +4,14 @@ public class Ex310 {
 
     public String deleteSpaceString(final String s) {
         StringBuffer result = new StringBuffer();
-        char ch = ' ';
 
         for (int i = 0; i < s.length() - 1; i++) {
-            ch = s.charAt(i);
-            if (ch == ' ' && s.charAt(i + 1) == ' ') {
+            if (s.charAt(i) == ' ' && s.charAt(i + 1) == ' ') {
                 continue;
             } else {
                 result.append(s.charAt(i));
             }
         }
-
         return result.toString().trim();
     }
 }
