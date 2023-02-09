937b119456b3e39008b9d03ab7701b0a95a1c823
hobathanh <hobathanh201@gmail.com>
2023-02-09 16:06:41 +0700

    fix conversation

--- src/main/java/com/bathanh/exercise/basic/string/Ex306.java
+++ src/main/java/com/bathanh/exercise/basic/string/Ex306.java
@@ -3,24 +3,20 @@ package com.bathanh.exercise.basic.string;
 public class Ex306 {
 
     public int calculateBarcode(final String n) {
+        int sum = 0;
+
         if (n.length() != 12) {
             throw new IllegalArgumentException();
         }
-        int verify = 0;
-        int sumOld = 0;
-        int sumEven = 0;
+
         for (int i = 0; i < n.length(); i++) {
             if (i % 2 == 0) {
-                sumOld += Integer.parseInt(n.charAt(i) + "");
+                sum += Integer.parseInt(n.charAt(i) + "");
             } else {
-                sumEven += Integer.parseInt(n.charAt(i) + "");
+                sum += 3 * Integer.parseInt(n.charAt(i) + "");
             }
         }
-        for (int i = 0; i < 10; i++) {
-            if ((sumOld + i + 3 * sumEven) % 10 == 0) {
-                verify = i;
-            }
-        }
-        return verify;
+
+        return 10 - (sum % 10);
     }
 }
