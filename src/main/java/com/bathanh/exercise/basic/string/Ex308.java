5559a2a24d24bf22eb5fd4eece7d13a0bb0569d6
hobathanh <hobathanh201@gmail.com>
2023-02-09 14:12:16 +0700

    add ex308

---
+++ src/main/java/com/bathanh/exercise/basic/string/Ex308.java
@@ -0,0 +1,17 @@
+package com.bathanh.exercise.basic.string;
+
+public class Ex308 {
+
+    public boolean checkString(final String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count += 1;
+            }
+            if (s.charAt(i) == ')') {
+                count -= 1;
+            }
+        }
+        return (count == 0);
+    }
+}
---
+++ src/test/java/com/bathanh/exercise/basic/string/Ex308Test.java
@@ -0,0 +1,22 @@
+package com.bathanh.exercise.basic.string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class Ex308Test {
+
+    @Test
+    void checkString_Ok() {
+        final var ex308 = new Ex308();
+
+        assertTrue(ex308.checkString("()()()"));
+        assertTrue(ex308.checkString("(())"));
+        assertTrue(ex308.checkString("(((())))()()"));
+        assertFalse(ex308.checkString("("));
+        assertFalse(ex308.checkString(")"));
+        assertFalse(ex308.checkString("()("));
+        assertFalse(ex308.checkString(")()"));
+    }
+}
