0f9608fd54a257cc26b78f745db7723d80f8e1ae
hobathanh <hobathanh201@gmail.com>
2023-02-09 10:05:58 +0700

    create and add Ex306

--- src/main/java/com/bathanh/exercise/basic/string/Ex306.java
+++ src/main/java/com/bathanh/exercise/basic/string/Ex306.java
@@ -1,2 +1,26 @@
-package com.bathanh.exercise.basic.string;public class Ex306 {
+package com.bathanh.exercise.basic.string;
+
+public class Ex306 {
+
+    public int calculateBarcode(final String n) {
+        if (n.length() != 12) {
+            throw new IllegalArgumentException();
+        }
+        int verify = 0;
+        int sumOld = 0;
+        int sumEven = 0;
+        for (int i = 0; i < n.length(); i++) {
+            if (i % 2 == 0) {
+                sumOld += Integer.parseInt(n.charAt(i) + "");
+            } else {
+                sumEven += Integer.parseInt(n.charAt(i) + "");
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            if ((sumOld + i + 3 * sumEven) % 10 == 0) {
+                verify = i;
+            }
+        }
+        return verify;
+    }
 }
---
+++ src/test/java/com/bathanh/exercise/basic/string/Ex306Test.java
@@ -0,0 +1,18 @@
+package com.bathanh.exercise.basic.string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class Ex306Test {
+
+    @Test
+    void calculateBarcode() {
+        final var ex306 = new Ex306();
+
+        assertThrows(IllegalArgumentException.class, () -> ex306.calculateBarcode("1213213"));
+        assertEquals(4, ex306.calculateBarcode("893850597419"));
+        assertEquals(5, ex306.calculateBarcode("123456789000"));
+    }
+}
