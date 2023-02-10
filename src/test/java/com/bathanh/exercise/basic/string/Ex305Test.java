b06daf2c5fa40cb6457be9cb95715007c411f724
hobathanh <hobathanh201@gmail.com>
2023-02-10 16:21:16 +0700

    fix conversation

--- src/test/java/com/bathanh/exercise/basic/string/Ex305Test.java
+++ src/test/java/com/bathanh/exercise/basic/string/Ex305Test.java
@@ -2,7 +2,8 @@ package com.bathanh.exercise.basic.string;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class Ex305Test {
 
@@ -10,9 +11,9 @@ class Ex305Test {
     void checkBarcode() {
         final var ex305 = new Ex305();
 
-        assertEquals(false, ex305.checkBarcode("123456789123456789"));
-        assertEquals(false, ex305.checkBarcode("12345"));
-        assertEquals(false, ex305.checkBarcode("1234567891234"));
-        assertEquals(true, ex305.checkBarcode("8938505974194"));
+        assertFalse(ex305.checkBarcode("123456789123456789"));
+        assertFalse(ex305.checkBarcode("12345"));
+        assertFalse(ex305.checkBarcode("1234567891234"));
+        assertTrue(ex305.checkBarcode("8938505974194"));
     }
 }
