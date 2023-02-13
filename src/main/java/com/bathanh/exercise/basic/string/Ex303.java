069ce28ce6e577dc1186a1f6c1837c50722ef424
hobathanh <hobathanh201@gmail.com>
2023-02-13 15:58:33 +0700

    fix conversation

--- src/main/java/com/bathanh/exercise/basic/string/Ex303.java
+++ src/main/java/com/bathanh/exercise/basic/string/Ex303.java
@@ -5,22 +5,16 @@ public class Ex303 {
     public int sumAllNumberString(final String s) {
         int sum = 0;
         int number = 0;
-        int j = 0;
-        int i = 0;
-        while (i < s.length()) {
-            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-                number = s.charAt(i) - '0';
-                for (j = i + 1; j < s.length(); j++) {
-                    if (s.charAt(j) < '0' || s.charAt(j) > '9') {
-                        break;
-                    }
-                    number = number * 10 + s.charAt(j) - '0';
-                }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                number = number * 10 + s.charAt(i) - '0';
+            } else {
                 sum += number;
-                i = j;
+                number = 0;
             }
-            i++;
         }
-        return sum;
+
+        return sum + number;
     }
 }
