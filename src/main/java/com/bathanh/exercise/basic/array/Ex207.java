51faf8c24ae35bcd0fe27e7619884713ecff811a
hobathanh <hobathanh201@gmail.com>
2023-02-13 09:32:41 +0700

    fix conversation

--- src/main/java/com/bathanh/exercise/basic/array/Ex207.java
+++ src/main/java/com/bathanh/exercise/basic/array/Ex207.java
@@ -2,32 +2,34 @@ package com.bathanh.exercise.basic.array;
 
 public class Ex207 {
 
-    public int[] sortArrayAsc(final int[] array, final int a) {
+    public int[] insertToSortedArray(final int[] array, final int a) {
         int[] newArray = new int[array.length + 1];
         int index = indexArray(array, a);
-        int j = 0;
-        for (int i = 0; i <= newArray.length - 1; i++) {
-            if (i == index) {
-                newArray[index] = a;
-            } else {
-                newArray[i] = array[j++];
-            }
+
+        for (int i = 0; i < index; i++) {
+            newArray[i] = array[i];
+        }
+        newArray[index] = a;
+        for (int j = index + 1; j <= newArray.length - 1; j++) {
+            newArray[j] = array[j - 1];
         }
         return newArray;
     }
 
     public int indexArray(final int[] array, final int a) {
-        int low = 0, high = array.length - 1;
+        int low = 0;
+        int high = array.length - 1;
         while (low <= high) {
-            int m = low + (high - low) / 2;
+            final int middle = low + (high - low) / 2;
 
-            if (array[m] == a) {
-                return m;
+            if (array[middle] == a) {
+                return middle;
             }
-            if (array[m] < a)
-                low = m + 1;
-            else
-                high = m - 1;
+            if (array[middle] < a) {
+                low = middle + 1;
+            } else {
+                high = middle - 1;
+            }
         }
         return low;
     }
--- src/test/java/com/bathanh/exercise/basic/array/Ex207Test.java
+++ src/test/java/com/bathanh/exercise/basic/array/Ex207Test.java
@@ -11,12 +11,12 @@ class Ex207Test {
     void sortArrayAsc_Ok() {
         final var ex207 = new Ex207();
 
-        assertArrayEquals(new int[]{1, 2, 3, 6, 7}, ex207.sortArrayAsc(new int[]{1, 2, 6, 7}, 3));
-        assertArrayEquals(new int[]{2, 3, 5, 6, 7}, ex207.sortArrayAsc(new int[]{3, 5, 6, 7}, 2));
-        assertArrayEquals(new int[]{1, 2, 2, 3, 6}, ex207.sortArrayAsc(new int[]{1, 2, 3, 6}, 2));
-        assertArrayEquals(new int[]{1, 2, 3, 6, 8}, ex207.sortArrayAsc(new int[]{1, 2, 3, 6}, 8));
-        assertArrayEquals(new int[]{1, 2}, ex207.sortArrayAsc(new int[]{1}, 2));
-        assertArrayEquals(new int[]{1}, ex207.sortArrayAsc(new int[]{}, 1));
+        assertArrayEquals(new int[]{1, 2, 3, 6, 7}, ex207.insertToSortedArray(new int[]{1, 2, 6, 7}, 3));
+        assertArrayEquals(new int[]{2, 3, 5, 6, 7}, ex207.insertToSortedArray(new int[]{3, 5, 6, 7}, 2));
+        assertArrayEquals(new int[]{1, 2, 2, 3, 6}, ex207.insertToSortedArray(new int[]{1, 2, 3, 6}, 2));
+        assertArrayEquals(new int[]{1, 2, 3, 6, 8}, ex207.insertToSortedArray(new int[]{1, 2, 3, 6}, 8));
+        assertArrayEquals(new int[]{1, 2}, ex207.insertToSortedArray(new int[]{1}, 2));
+        assertArrayEquals(new int[]{1}, ex207.insertToSortedArray(new int[]{}, 1));
     }
 
     @Test
@@ -25,6 +25,7 @@ class Ex207Test {
 
         assertEquals(2, ex207.indexArray(new int[]{1, 2, 6, 7}, 3));
         assertEquals(0, ex207.indexArray(new int[]{3, 5, 6, 7}, 2));
+        assertEquals(0, ex207.indexArray(new int[]{3, 5, 6, 7}, -3));
         assertEquals(1, ex207.indexArray(new int[]{1, 2, 3, 6}, 2));
         assertEquals(4, ex207.indexArray(new int[]{1, 2, 3, 6}, 8));
         assertEquals(0, ex207.indexArray(new int[]{}, 1));
