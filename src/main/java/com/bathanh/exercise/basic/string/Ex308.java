b13ae42c0518a10a3a9ddbc7e6da14667ef465bb
hobathanh <hobathanh201@gmail.com>
2023-02-13 13:43:37 +0700

    fix conversation

--- src/main/java/com/bathanh/exercise/basic/string/Ex308.java
+++ src/main/java/com/bathanh/exercise/basic/string/Ex308.java
@@ -5,11 +5,14 @@ public class Ex308 {
     public boolean checkString(final String s) {
         int count = 0;
         for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                count -= 1;
+            }
             if (s.charAt(i) == '(') {
                 count += 1;
             }
-            if (s.charAt(i) == ')') {
-                count -= 1;
+            if (count < 0) {
+                return false;
             }
         }
         return (count == 0);
