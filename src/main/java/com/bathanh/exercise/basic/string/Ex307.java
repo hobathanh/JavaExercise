1eba3c2d0982d91463a0bca34438f4c7a72959c7
hobathanh <hobathanh201@gmail.com>
2023-02-17 09:48:46 +0700

    fix request

--- src/main/java/com/bathanh/exercise/basic/string/Ex307.java
+++ src/main/java/com/bathanh/exercise/basic/string/Ex307.java
@@ -2,26 +2,18 @@ package com.bathanh.exercise.basic.string;
 
 public class Ex307 {
 
-    public int findIndexStringInString(final String s1, final String s2) {
-
-        for (int i = 0; i <= Math.abs(s1.length() - s2.length()); i++) {
-            if (s1.length() - s2.length() >= 0) {
-                if (isSubString(s1, s2, i)) {
-                    return i;
-                }
-            } else {
-                if (isSubString(s2, s1, i)) {
-                    return i;
-                }
+    public int findIndexOfString(final String string, final String pattrenString) {
+        for (int index = 0; index <= string.length() - pattrenString.length(); index++) {
+            if (indexOfString(string, pattrenString, index)) {
+                return index;
             }
-
         }
         return -1;
     }
 
-    public boolean isSubString(final String inputString, final String subString, final int index) {
-        for (int i = 0; i < subString.length(); i++) {
-            if (inputString.charAt(index + i) != subString.charAt(i)) {
+    public boolean indexOfString(String string, String pattrenString, int index) {
+        for (int i = 0; i < pattrenString.length(); i++) {
+            if (string.charAt(index + i) != pattrenString.charAt(i)) {
                 return false;
             }
         }
--- src/test/java/com/bathanh/exercise/basic/string/Ex307Test.java
+++ src/test/java/com/bathanh/exercise/basic/string/Ex307Test.java
@@ -10,11 +10,9 @@ class Ex307Test {
     void searchStringInString() {
         final var ex307 = new Ex307();
 
-        assertEquals(2, ex307.findIndexStringInString("oiutytre", "utyt"));
-        assertEquals(2, ex307.findIndexStringInString("utyt", "oiutytredajhfsafjvajf"));
-        assertEquals(1, ex307.findIndexStringInString("toabcd", "oa"));
-        assertEquals(0, ex307.findIndexStringInString("todyyyabcd", "t"));
-        assertEquals(0, ex307.findIndexStringInString("t", "todyyyabcd"));
-        assertEquals(-1, ex307.findIndexStringInString("dadasda", "k"));
+        assertEquals(2, ex307.findIndexOfString("ho ba thanh", " "));
+        assertEquals(1, ex307.findIndexOfString("ho ba thanh", "o"));
+        assertEquals(0, ex307.findIndexOfString("ho ba thanh", "ho"));
+        assertEquals(-1, ex307.findIndexOfString("ho ba thanh", "tan"));
     }
 }
