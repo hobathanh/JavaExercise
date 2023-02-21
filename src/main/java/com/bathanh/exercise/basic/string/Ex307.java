e97ac1d8e4f64c35b29e4313658f71afa40539ec
hobathanh <hobathanh201@gmail.com>
2023-02-21 10:29:03 +0700

    fix conversation

--- src/main/java/com/bathanh/exercise/basic/string/Ex307.java
+++ src/main/java/com/bathanh/exercise/basic/string/Ex307.java
@@ -2,18 +2,18 @@ package com.bathanh.exercise.basic.string;
 
 public class Ex307 {
 
-    public int findIndexOfString(final String string, final String pattrenString) {
-        for (int index = 0; index <= string.length() - pattrenString.length(); index++) {
-            if (indexOfString(string, pattrenString, index)) {
+    public int findIndexOfString(final String inputString, final String subString) {
+        for (int index = 0; index <= inputString.length() - subString.length(); index++) {
+            if (isSubStringAt(inputString, subString, index)) {
                 return index;
             }
         }
         return -1;
     }
 
-    public boolean indexOfString(String string, String pattrenString, int index) {
-        for (int i = 0; i < pattrenString.length(); i++) {
-            if (string.charAt(index + i) != pattrenString.charAt(i)) {
+    public boolean isSubStringAt(String inputString, String subString, int index) {
+        for (int i = 0; i < subString.length(); i++) {
+            if (inputString.charAt(index + i) != subString.charAt(i)) {
                 return false;
             }
         }
